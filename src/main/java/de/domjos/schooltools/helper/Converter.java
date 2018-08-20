6dc98586cf93a7a94796dcc07d36bd9c960a804f
domjos1994 <developing@domjos.de>
2018-08-20 19:26:42 +0200

    solve bug 0000009

--- src/main/java/de/domjos/schooltools/helper/Converter.java
+++ src/main/java/de/domjos/schooltools/helper/Converter.java
@@ -47,12 +47,14 @@ public class Converter {
         if(date.isEmpty()) {
             return null;
         } else {
-            if(Pattern.matches("^(29-02-(2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26]))))$|^((0[1-9]|[12][0-9]|3[01])-(0[13578]|10|12)-((19|2[0-9])[0-9]{2})$|^(0[1-9]|[12][0-9]|30)-(0[469]|11)-((19|2[0-9])[0-9]{2}))$", date)) {
-                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
-                return sdf.parse(date);
-            } else if(Pattern.matches("^(29\\.02\\.(2000|2400|2800|(19|2[0-9](0[48]|[2468][048]|[13579][26]))))$|^((0[1-9]|[12][0-9]|3[01])\\.(0[13578]|10|12)\\.((19|2[0-9])[0-9]{2})$|^(0[1-9]|[12][0-9]|30)\\.(0[469]|11)\\.((19|2[0-9])[0-9]{2}))$", date)) {
-                SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
-                return sdf.parse(date);
+            if(Pattern.matches("^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$", date)) {
+                if(date.contains("-")) {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH);
+                    return sdf.parse(date);
+                } else {
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+                    return sdf.parse(date);
+                }
             }
             return null;
         }
