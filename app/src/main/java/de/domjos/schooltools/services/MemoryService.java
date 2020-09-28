aa8d23a145f72b74051e0e6e52ecca1a8735ca36
domjos1994 <developing@domjos.de>
2020-09-28 14:21:44 +0200

    solve bug 0000743

--- app/src/main/java/de/domjos/schooltools/services/MemoryService.java
+++ app/src/main/java/de/domjos/schooltools/services/MemoryService.java
@@ -15,6 +15,7 @@ import android.content.Intent;
 import de.domjos.customwidgets.utils.MessageHelper;
 import de.domjos.schooltools.R;
 import de.domjos.schooltools.activities.*;
+import de.domjos.schooltools.helper.SQLite;
 import de.domjos.schooltoolslib.model.Memory;
 import de.domjos.schooltoolslib.model.learningCard.LearningCardQuery;
 import de.domjos.schooltoolslib.model.learningCard.LearningCardQueryTraining;
@@ -34,44 +35,47 @@ public class MemoryService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        for(Memory memory : MainActivity.globals.getSqLite().getCurrentMemories()) {
-            try {
-                if(Helper.compareDateWithCurrentDate(ConvertHelper.convertStringToDate(memory.getDate(), this.getApplicationContext()))) {
-                    Intent linkedIntent = null;
-                    switch (memory.getType()) {
-                        case Note:
-                            linkedIntent = new Intent(this.getApplicationContext(), NoteActivity.class);
-                            break;
-                        case Test:
-                            linkedIntent = new Intent(this.getApplicationContext(), MarkEntryActivity.class);
-                            linkedIntent.putExtra("id", memory.getId());
-                            linkedIntent.putExtra("enabled", false);
-                            break;
-                        case toDo:
-                            linkedIntent = new Intent(this.getApplicationContext(), ToDoActivity.class);
-                            break;
-                        case timerEvent:
-                            linkedIntent = new Intent(this.getApplicationContext(), TimerActivity.class);
-                            linkedIntent.putExtra("date", memory.getDate());
-                            break;
-                    }
+        SQLite sqLite = MainActivity.globals.getSqLite();
+        if(sqLite != null) {
+            for(Memory memory : sqLite.getCurrentMemories()) {
+                try {
+                    if(Helper.compareDateWithCurrentDate(ConvertHelper.convertStringToDate(memory.getDate(), this.getApplicationContext()))) {
+                        Intent linkedIntent = null;
+                        switch (memory.getType()) {
+                            case Note:
+                                linkedIntent = new Intent(this.getApplicationContext(), NoteActivity.class);
+                                break;
+                            case Test:
+                                linkedIntent = new Intent(this.getApplicationContext(), MarkEntryActivity.class);
+                                linkedIntent.putExtra("id", memory.getId());
+                                linkedIntent.putExtra("enabled", false);
+                                break;
+                            case toDo:
+                                linkedIntent = new Intent(this.getApplicationContext(), ToDoActivity.class);
+                                break;
+                            case timerEvent:
+                                linkedIntent = new Intent(this.getApplicationContext(), TimerActivity.class);
+                                linkedIntent.putExtra("date", memory.getDate());
+                                break;
+                        }
 
-                    MessageHelper.showNotification(this.getApplicationContext(), memory.getTitle(), memory.getDescription(), R.mipmap.ic_launcher_round, linkedIntent, 99);
-                } else {
+                        MessageHelper.showNotification(this.getApplicationContext(), memory.getTitle(), memory.getDescription(), R.mipmap.ic_launcher_round, linkedIntent, 99);
+                    } else {
+                        if(MainActivity.globals.getUserSettings().isDeleteMemories()) {
+                            MainActivity.globals.getSqLite().deleteEntry("memories", "itemID=" + memory.getId());
+                        }
+                    }
+                } catch (Exception ex) {
+                    MessageHelper.printException(ex, R.mipmap.ic_launcher_round, getApplicationContext());
                     if(MainActivity.globals.getUserSettings().isDeleteMemories()) {
                         MainActivity.globals.getSqLite().deleteEntry("memories", "itemID=" + memory.getId());
                     }
                 }
-            } catch (Exception ex) {
-                MessageHelper.printException(ex, R.mipmap.ic_launcher_round, getApplicationContext());
-                if(MainActivity.globals.getUserSettings().isDeleteMemories()) {
-                    MainActivity.globals.getSqLite().deleteEntry("memories", "itemID=" + memory.getId());
-                }
             }
-        }
 
-        for(LearningCardQuery query : this.loadPeriodicLearningCardQueries()) {
-            MessageHelper.showNotification(this.getApplicationContext(), query.getTitle(), getApplicationContext().getString(R.string.learningCard_query_memory) + query.getTitle(), R.mipmap.ic_launcher_round, intent, 99);
+            for(LearningCardQuery query : this.loadPeriodicLearningCardQueries()) {
+                MessageHelper.showNotification(this.getApplicationContext(), query.getTitle(), getApplicationContext().getString(R.string.learningCard_query_memory) + query.getTitle(), R.mipmap.ic_launcher_round, intent, 99);
+            }
         }
     }
 
