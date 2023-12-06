d596ea20621631de225fbeee889495b1763f3196
domjos1994 <developing@domjos.de>
2023-12-06 17:12:05 +0100

    update version

---
+++ DBController/src/androidTest/java/de/domjos/schooltools/dbcontroller/ExampleInstrumentedTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017-2023  Dominic Joas
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
+package de.domjos.schooltools.dbcontroller;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("de.domjos.schooltools.dbcontroller.test", appContext.getPackageName());
+    }
+}
---
+++ DBController/src/test/java/de/domjos/schooltools/dbcontroller/ExampleUnitTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2017-2023  Dominic Joas
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ */
+
+package de.domjos.schooltools.dbcontroller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
